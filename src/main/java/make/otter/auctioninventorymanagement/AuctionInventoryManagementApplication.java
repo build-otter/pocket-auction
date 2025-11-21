76cc93816491bcbdba2a7a891bdd5e5ac66163cf
OortCloud <skintoodev@gmail.com>
2025-11-21 19:56:48 +0900

    project init

---
+++ src/main/java/make/otter/auctioninventorymanagement/AuctionInventoryManagementApplication.java
@@ -0,0 +1,13 @@
+package make.otter.auctioninventorymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionInventoryManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionInventoryManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/make/otter/auctioninventorymanagement/AuctionInventoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package make.otter.auctioninventorymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionInventoryManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
